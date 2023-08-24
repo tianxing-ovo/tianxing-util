7820f1861bed4e7c29973f99f5709d2f41a7d892
ltx <2050365570@qq.com>
2023-08-24 22:24:25 +0800

    升级版本

--- src/main/java/exceptions/CustomException.java
+++ src/main/java/exceptions/CustomException.java
@@ -7,7 +7,7 @@ public class CustomException extends RuntimeException {
 
     private final int code;
 
-    public CustomException(String message, int code) {
+    public CustomException(int code, String message) {
         super(message);
         this.code = code;
     }
