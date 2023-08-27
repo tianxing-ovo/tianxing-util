f37459d3683efdbc667fe94f7fce36b5f440e946
ltx <2050365570@qq.com>
2023-08-27 16:45:39 +0800

    update

--- src/main/java/common/R.java
+++ src/main/java/common/R.java
@@ -1,5 +1,6 @@
 package common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ import java.util.Map;
  */
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class R {
     private Integer code; //状态码
     private String msg; //消息
