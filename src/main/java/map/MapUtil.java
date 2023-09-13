14ed0f07320976fcc0119106f462d5d6f56b1fcf
ltx <2050365570@qq.com>
2023-09-14 07:52:18 +0800

    update

--- src/main/java/map/MapUtil.java
+++ src/main/java/map/MapUtil.java
@@ -2,7 +2,9 @@ package map;
 
 import lombok.SneakyThrows;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,24 @@ public class MapUtil {
             }
         }
         return t;
+    }
+
+    /**
+     * object转map
+     */
+    @SneakyThrows
+    public static Map<String, Object> objectToMap(Object o) {
+        // 保持元素的插入顺序
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        Class<?> clazz = o.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            String name = field.getName();
+            Object value = field.get(o);
+            map.put(name, value);
+        }
+        return map;
     }
 }
 
