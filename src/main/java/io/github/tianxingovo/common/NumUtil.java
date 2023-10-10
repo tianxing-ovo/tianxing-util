032e4857cb65a7cc704478e6b25bb8ac7d1217f3
ltx <2050365570@qq.com>
2023-10-10 20:43:27 +0800

    阿里云短信验证码发送

---
+++ src/main/java/io/github/tianxingovo/common/NumUtil.java
@@ -0,0 +1,17 @@
+package io.github.tianxingovo.common;
+
+import java.util.Random;
+
+/**
+ * 数字工具类
+ */
+public class NumUtil {
+    /**
+     * 获取n位随机整数
+     */
+    public static int randomNum(int n) {
+        int min = (int) Math.pow(10, n - 1);
+        int max = (int) Math.pow(10, n) - 1;
+        return new Random().nextInt(max - min + 1) + min;
+    }
+}
---
+++ src/main/java/io/github/tianxingovo/common/SMSUtil.java
@@ -0,0 +1,52 @@
+package io.github.tianxingovo.common;
+
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.tea.TeaException;
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.teautil.models.RuntimeOptions;
+import lombok.SneakyThrows;
+
+import static com.aliyun.teautil.Common.assertAsString;
+
+/**
+ * 阿里云短信验证码工具类
+ */
+public class SMSUtil {
+    public static void main(String[] args) {
+        sendMessage("tanhua交友", "SMS_276055333", "17107815436");
+    }
+
+    /**
+     * 发送短信验证码
+     */
+    public static void sendMessage(String signName, String templateCode, String phoneNumbers) {
+        // 从环境变量中读取ak和sk
+        String accessKeyId = System.getenv("ALIBABA_CLOUD_ACCESS_KEY_ID");
+        String accessKeySecret = System.getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET");
+        Client client = createClient(accessKeyId, accessKeySecret);
+        SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                .setSignName(signName)
+                .setTemplateCode(templateCode)
+                .setPhoneNumbers(phoneNumbers)
+                .setTemplateParam("{\"code\":\"" + NumUtil.randomNum(4) + "\"}");
+        RuntimeOptions runtimeOptions = new RuntimeOptions();
+        try {
+            client.sendSmsWithOptions(sendSmsRequest, runtimeOptions);
+        } catch (TeaException teaException) {
+            assertAsString(teaException.message);
+        } catch (Exception exception) {
+            TeaException teaException = new TeaException(exception.getMessage(), exception);
+            assertAsString(teaException.message);
+        }
+    }
+
+    @SneakyThrows
+    public static Client createClient(String accessKeyId, String accessKeySecret) {
+        Config config = new Config()
+                .setAccessKeyId(accessKeyId)
+                .setAccessKeySecret(accessKeySecret)
+                .setEndpoint("dysmsapi.aliyuncs.com");
+        return new Client(config);
+    }
+}
