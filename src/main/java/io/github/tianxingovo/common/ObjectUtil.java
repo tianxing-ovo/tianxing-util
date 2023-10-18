e80992e3ca19a6887cce751830abd11c708b8b40
ltx <2050365570@qq.com>
2023-10-18 23:56:10 +0800

    update

---
+++ src/main/java/io/github/tianxingovo/common/ObjectUtil.java
@@ -0,0 +1,38 @@
+package io.github.tianxingovo.common;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 对象工具类
+ */
+public class ObjectUtil {
+    /**
+     * 获取对象属性,收集到List中
+     */
+    public <T> List<Object> getProperty(Class<T> clazz, T t, List<String> fieldList) {
+        List<Field> list = Arrays.asList(clazz.getDeclaredFields());
+        // 获取指定属性
+        if (fieldList != null) {
+            list = list.stream().filter(field -> fieldList.contains(field.getName())).collect(Collectors.toList());
+        }
+        return list.stream().map(field -> {
+            field.setAccessible(true);
+            try {
+                return field.get(t);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * 获取对象的所有属性,收集到List中
+     */
+    public <T> List<Object> getProperty(Class<T> clazz, T t) {
+        return getProperty(clazz, t, null);
+    }
+}
