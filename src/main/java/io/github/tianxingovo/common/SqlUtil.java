e1175b04e43eb6fb362821916b535bbca02a2e9c
ltx <2050365570@qq.com>
2023-11-25 21:03:54 +0800

    sql生成工具

---
+++ src/main/java/io/github/tianxingovo/common/SqlUtil.java
@@ -0,0 +1,26 @@
+package io.github.tianxingovo.common;
+
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * 数据库工具类,用于生成SQL语句
+ */
+public class SqlUtil {
+
+    /**
+     * 生成批量更新语句
+     *
+     * @param updateField 需要更新的字段
+     * @param field       case的字段
+     */
+    public static StringJoiner batchUpdateSql(String tableName, String updateField, String field, Map<Integer, String> map) {
+        String prefix = String.format("update %s set %s = case %s ", tableName, updateField, field);
+        StringJoiner joiner = new StringJoiner(" ", prefix, " end");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            String s = String.format("when %d then %s", entry.getKey(), entry.getValue());
+            joiner.add(s);
+        }
+        return joiner;
+    }
+}
